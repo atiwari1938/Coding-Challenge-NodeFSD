858c362bbcbd1159eb9472e29ef63c572f6439c4
Ankit Tiwari <atiwari1938@gmail.com>
2024-05-07 16:37:05 +0530

    Final commit

--- controllers/DoctorController.js
+++ controllers/DoctorController.js
@@ -1,10 +1,14 @@
 const Doctor = require('../models/DoctorModel');
+const logger=require('../utils/logger');
 
 exports.getAllDoctors = async (req, res) => {
   try {
+    //console.log(abc.property);//to check error.log working
     const doctors = await Doctor.findAll();
+    logger.info("Get all doctors",doctors);
     res.json(doctors);
   } catch (error) {
+    logger.error("Error fetching doctors:",error);
     console.error("Error fetching doctors:",error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
@@ -12,12 +16,16 @@ exports.getAllDoctors = async (req, res) => {
 
 exports.getDoctorById = async (req, res) => {
   try {
+    
     const doctor = await Doctor.findByPk(req.params.doctorId);
     if (!doctor) {
+        logger.info("No Doctors found");
       return res.status(404).json({ message: 'Doctor not found' });
     }
+    logger.info("Get doctor by id:",doctor);
     res.json(doctor);
   } catch (error) {
+    logger.error("Error fetching doctor by id:",error);
     console.error('Error fetching doctor by id:',error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
@@ -28,9 +36,11 @@ exports.createDoctor = async (req, res) => {
   const doctor = new Doctor(doctorData);
   try {
     await doctor.save();
+    logger.info("Doctor created successfully",doctor);
     res.status(201).json({ message: 'Doctor created successfully',doctor });
   } catch (error) {
     console.error('Error creating doctor:',error);
+    logger.error("Error creating doctor:",error);
     res.status(400).json({ error: '400 Bad Request' });
   }
 };
@@ -52,9 +62,11 @@ exports.updateDoctor = async (req, res) => {
       doctor.degree = degree;
   
       await doctor.save();
+      logger.info("Doctor updated succesfully",doctor);
       console.log("Doctor Updated successfully");
       res.status(200).json({ message: "Doctor Updated successfully", doctor });
     } catch (error) {
+        logger.error("Error in updating doctor:",error);
       console.error('Error in updating doctor:', error);
       res.status(500).json({ error: 'Internal Server Error' });
     }
@@ -68,8 +80,10 @@ exports.deleteDoctor = async (req, res) => {
         return res.status(404).json({ message: 'Doctor not found' });
       }
       await doctor.destroy();
+      logger.info("Doctor Deleted ",doctor);
       res.json({ message: 'Doctor deleted successfully',doctor });
     } catch (error) {
+        logger.error("Error Deleting Doctor:",error);
         console.error('Error Deleting Doctor:',error);
       res.status(500).json({ error: 'Internal Server Error' });
     }
--- middleware/validation.js
+++ middleware/validation.js
@@ -2,8 +2,12 @@
 const { check, validationResult } = require('express-validator');
 
 exports.validateDoctor = [
-  check('email').isEmail().withMessage('Invalid email'),
-  check('contactNumber').isMobilePhone('en-IN').withMessage('Invalid mobile number mobile number shoukd follow en-IN format'),
+  check('email')
+  .isEmail()
+  .withMessage('Invalid email'),
+  check('contactNumber')
+  .isMobilePhone('en-IN')
+  .withMessage('Invalid mobile number mobile number shoukd follow en-IN format'),//check if the value is a valid mobile phone number in the Indian format.
   (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
